b643eaad608005211ae0bf51944bb52ea97f6672
xg347979907 <112589726+xg347979907@users.noreply.github.com>
2026-03-05 21:48:37 -0800

    Update main.py add render_template for index.html

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #creates instance of flask app
 app = Flask(__name__)
 
 #root url, when '/' is accessed 
 @app.route("/")
-
+def index():
+    return render_template("index.html")
 #When user visits the website
 def home():
     return "Scheduling Webstie Loading..."
